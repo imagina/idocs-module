fffa9beb6ece177f15a8ce0a7370de4e689b3287
leopoldo <leopoldo.mendez@outlook.com>
2018-11-26 10:49:37 -0400

    user permission to documents
    in here the permission to see a document is attached to a user

---
+++ Database/Migrations/2018_10_03_194604_create_idocs_doc_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIdocsDocUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idocs__doc__user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('doc_id')->unsigned();
+            $table->integer('user_allowed_id')->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('idocs__doc__user');
+    }
+}
